839f81d0da28f0c903555aef04a36752e8d44d0d
Daniel <dsedaca@hotmail.com>
2023-11-21 14:07:58 -0500

    Made file adjustments

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import SearchBar from '../SearchBar/SearchBar';
+import React from 'react';
+
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+function SearchBar() {
+    return (
+        <>
+            <p>This is where the search bar is going.</p>
+        </>
+    )
+}
+
+export default SearchBar;
