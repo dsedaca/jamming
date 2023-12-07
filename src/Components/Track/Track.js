2a7df74d4f4277cf3af67c193c55496c96dfdf70
Daniel <dsedaca@hotmail.com>
2023-12-07 16:54:51 -0500

    Implemented some imports and exports

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Track.css';
+
+function Track() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Track;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+
+function TrackList() {
+    return (
+        <div className='TrackList'>
+            <Track />
+        </div>
+    );
+}
+
+export default TrackList;
